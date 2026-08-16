74b675b9038394e68522446dc0baf6dba1af6fda
Mohammad-Reza Farahi <rqlzienc@gmail.com>
2026-08-16 15:41:42 +0330

    image dataset

--- src/datasets.py
+++ src/datasets.py
@@ -1,30 +1,48 @@
 # Dependencies
+from typing import Optional, Literal
 from pathlib import Path
 import pandas as pd
 from torch.utils.data import Dataset
 
 
 class BirdImageDataset(Dataset):
-    def __init__(self, img_dir: Path, ann_file: str | Path, device):
+    def __init__(
+        self,
+        img_dir: Path,
+        ann_file: str | Path,
+        dst: Optional[Literal['Bird-MY10', 'Bird-SEA10']] = None,
+        device: Optional[str]=...,
+    ):
+        # Image folder
         self.img_dir = img_dir
 
+        # Annotations file
         self.ann = pd.read_csv(ann_file)
         self.ann["label"] = self.ann["label"].astype("category").cat.codes
-        self.ann["dataset"] = self.ann["dataset"].astype("category").cat.codes
 
+        if dst is not None:
+            self.ann = self.ann[self.ann["dataset"] == dst]
+            self.ann = self.ann.reset_index(drop=True)
+
+        self.ann = self.ann.drop(columns="dataset")
+
+        # Device cuda/cpu
+        self.device = device
 
 
     def __getitem__(self, idx: int):
         pass
 
-    def __len__(self):
-        pass
+
+    def __len__(self) -> int:
+        return len(self.ann)
 
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("dataset/image-annotations.csv")
-    df["label"] = df["label"].astype("category").cat.codes
-    df["dataset"] = df["dataset"].astype("category").cat.codes
-    print(df.head())
-    print(df.info())
+    mydataset = BirdImageDataset(
+        img_dir=Path('dataset/image'),
+        ann_file=Path('dataset/image-annotations.csv'),
+    )
+
+    print(len(mydataset))
