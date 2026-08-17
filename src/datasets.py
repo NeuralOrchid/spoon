3e49e3eca8588e9b5e436a37d60a4482ab039fe9
Mohammad-Reza Farahi <rqlzienc@gmail.com>
2026-08-17 17:38:45 +0330

    image only dataset

--- src/datasets.py
+++ src/datasets.py
@@ -2,7 +2,9 @@
 from typing import Optional, Literal
 from pathlib import Path
 import pandas as pd
+from PIL import Image, ImageOps
 from torch.utils.data import Dataset
+from torchvision import transforms as T
 
 
 class BirdImageDataset(Dataset):
@@ -10,9 +12,8 @@ class BirdImageDataset(Dataset):
         self,
         img_dir: Path,
         ann_file: str | Path,
-        transformation,
+        train: bool = True,
         dst: Optional[Literal['Bird-MY10', 'Bird-SEA10']] = None,
-        device: Optional[str]='cpu',
     ):
         # Image folder
         self.img_dir = img_dir
@@ -27,19 +28,69 @@ class BirdImageDataset(Dataset):
 
         self.ann = self.ann.drop(columns="dataset")
 
-        # Device cuda/cpu
-        self.device = device
-        self.transformation = transformation.to(self.device)
+        if train:
+            self.transform = T.Compose([
+                T.Resize(384),
+                T.RandomResizedCrop(256, (0.8, 1.0), (0.95, 1.05)),
+                T.RandomHorizontalFlip(),
+
+                T.ColorJitter(
+                    brightness=0.4,
+                    contrast=0.4,
+                    saturation=0.2,
+                    hue=0.1
+                ),
+
+                T.ToTensor(),
+
+                T.Normalize(
+                    mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225)
+                ),
+            ])
+            
+        else:
+            self.transform = T.Compose([
+                T.Resize(256),
+                T.ToTensor(),
+                T.Normalize(
+                    mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225)
+                ),
+            ])
 
 
     def __len__(self) -> int:
         return len(self.ann)
 
 
+    def _pad2square(self, img: Image.Image) -> Image.Image:
+        w, h = img.size
+
+        if w == h:
+            return img
+
+        m = max(w, h)
+
+        pl = (m - w) // 2
+        pt = (m - h) // 2
+        pr = m - w - pl
+        pb = m - h - pt
+
+        return ImageOps.expand(img, border=(pl, pt, pr, pb))
+        
+
     def __getitem__(self, idx: int):
-        pass
+        image_path = self.img_dir / self.ann.loc[idx, "file"]
+        label = int(self.ann.loc[idx, "label"])
 
+        image = Image.open(image_path).convert("RGB")
+        image = self._pad2square(image)
 
+        image = self.transform(image)
+
+        return image, label
+    
 
 if __name__ == "__main__":
     mydataset = BirdImageDataset(
@@ -47,4 +98,18 @@ if __name__ == "__main__":
         ann_file=Path('dataset/image-annotations.csv'),
     )
 
-    print(len(mydataset))
+    img, _ = mydataset[0]
+
+    import torch
+    import matplotlib.pyplot as plt
+
+    mean = torch.tensor([0.485, 0.456, 0.406])
+    std  = torch.tensor([0.229, 0.224, 0.225])
+
+    img = img * std[:, None, None] + mean[:, None, None]
+    img = img.permute(1, 2, 0)
+    img = img.clamp(0, 1)
+
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
