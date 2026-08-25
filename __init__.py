5b66a7850a8ac14ab2309a542fb04e89a70aa71b
Mohammad-Reza Farahi <rqlzienc@gmail.com>
2026-08-25 19:03:21 +0330

    2026-08-25 19:03

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from .image_dataset import BirdImageDataset
 from .audio_dataset import BirdAudioDataset
-from .unpaired_dataset import UnpairedDataset
 from .paired_dataset import PairedDataset, AudioTransform
 
 from .mobilevit import MobileViT_XXS
@@ -8,6 +7,5 @@ from .mobilevit import MobileViT_XXS
 from .train import (
     ImageTrainer,
     AudioTrainer,
-    UnpairedTrainer,
     PairedTrainer,
 )
